7d47cbf57d90ad957e947b40824e4bf34f8e6209
Jeff Anderson <jeff@jeff-anderson.com>
2020-07-26 20:10:34 -0400

    Changes from chapter 6 - adding a database

--- src/main/java/com/spankinfresh/blog/domain/BlogPost.java
+++ src/main/java/com/spankinfresh/blog/domain/BlogPost.java
@@ -2,10 +2,6 @@ package com.spankinfresh.blog.domain;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import java.time.LocalDateTime;
 import javax.persistence.*;
 
