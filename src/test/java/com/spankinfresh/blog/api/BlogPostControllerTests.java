582c6b128c3e9a5c6c8ebfa21109401f0a026cfd
Jeff Anderson <jeff@jeff-anderson.com>
2020-08-16 19:03:50 -0400

    Additional content for mocking and stubbing.

--- src/test/java/com/spankinfresh/blog/api/BlogPostControllerTests.java
+++ src/test/java/com/spankinfresh/blog/api/BlogPostControllerTests.java
@@ -1,18 +1,25 @@
 package com.spankinfresh.blog.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.spankinfresh.blog.data.BlogPostRepository;
 import com.spankinfresh.blog.domain.BlogPost;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Date;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -21,33 +28,43 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 public class BlogPostControllerTests {
 
+  @MockBean
+  private BlogPostRepository mockRepository;
+
   private static final String RESOURCE_URI = "/api/articles";
   private final ObjectMapper mapper = new ObjectMapper();
   private static final BlogPost testPosting =
-    new BlogPost(1L, "category", null, "title", "content");
+    new BlogPost(0L, "category", null, "title", "content");
+  private static final BlogPost savedPosting =
+    new BlogPost(1, "category", LocalDateTime.now(), "title", "content");
 
   @Test
   @DisplayName("T01 - POST accepts and returns blog post representation")
   public void postCreatesNewBlogEntry_Test(@Autowired MockMvc mockMvc)
     throws Exception {
-    System.out.println(mapper.writeValueAsString(testPosting));
+    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm");
+    //when(mockRepository.save(refEq(testPosting, "datePosted"))).thenReturn(savedPosting);
+    when(mockRepository.save(any(BlogPost.class))).thenReturn(savedPosting);
     MvcResult result = mockMvc.perform(post(RESOURCE_URI)
       .contentType(MediaType.APPLICATION_JSON)
       .content(mapper.writeValueAsString(testPosting)))
       .andExpect(status().isCreated())
       .andExpect(jsonPath(
-        "$.id").value(testPosting.getId()))
+        "$.id").value(savedPosting.getId()))
       .andExpect(jsonPath(
-        "$.title").value(testPosting.getTitle()))
+        "$.title").value(savedPosting.getTitle()))
       .andExpect(jsonPath(
-        "$.category").value(testPosting.getCategory()))
+        "$.datePosted").value(simpleDateFormat.format(savedPosting.getDatePosted())))
+      .andExpect(jsonPath(
+        "$.category").value(savedPosting.getCategory()))
       .andExpect(
-        jsonPath("$.content").value(testPosting.getContent()))
+        jsonPath("$.content").value(savedPosting.getContent()))
       .andReturn();
     MockHttpServletResponse mockResponse = result.getResponse();
     assertEquals(String.format(
-      "http://localhost/api/articles/%d", testPosting.getId()),
+      "http://localhost/api/articles/%d", savedPosting.getId()),
       mockResponse.getHeader("Location"));
+    verify(mockRepository, times(1)).save(refEq(testPosting, "datePosted"));
   }
 
 }
