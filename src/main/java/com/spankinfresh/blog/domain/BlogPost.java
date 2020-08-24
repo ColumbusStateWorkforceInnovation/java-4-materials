bbbf29da033f3deae5d83a8b0dfcbf9ea84cf54a
Jeff Anderson <jeff@jeff-anderson.com>
2020-08-23 20:04:03 -0400

    Updated validation chapter

--- src/main/java/com/spankinfresh/blog/domain/BlogPost.java
+++ src/main/java/com/spankinfresh/blog/domain/BlogPost.java
@@ -2,8 +2,13 @@ package com.spankinfresh.blog.domain;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
-import javax.persistence.*;
 
 @Entity
 public class BlogPost {
@@ -11,10 +16,20 @@ public class BlogPost {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
-  private String category;
   @DateTimeFormat(iso=DateTimeFormat.ISO.DATE_TIME)
   private LocalDateTime datePosted;
+  @NotNull
+  @Size(min = 1, max = 200,
+    message = "Please enter a category name of up to 200 characters")
+  private String category;
+
+  @NotNull
+  @Size(min = 1, max = 200,
+    message = "Please enter a title up to 200 characters in length")
   private String title;
+
+  @NotNull
+  @Size(min = 1, max = 500000, message = "Content is required")
   private String content;
 
   public BlogPost() { }
